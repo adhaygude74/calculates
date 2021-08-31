d0bb575bc64940415ca2bb5a40c43a5cd3ad5750
Akshay Dhaygude <adhaygude74@gmail.com>
2021-08-31 20:14:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from "react";
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  let ak;
+       const [sets,funs]=useState("");                        
+                      useRef()
+          function handle(e)
+         {
+            funs(sets+(e.target.name));
+         }
+
+         function backs()
+         {
+          funs(sets.slice(0,sets.length-1));
+         }
+         function clears()
+         {
+          funs(sets="");
+         }
+
+         function cal()
+         {
+           try{
+           funs(eval(sets)+"")
+           }
+           catch(error)
+           {
+             funs("Error")
+           }
+         }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   {/* calculator  */}
+            <h1> Calculator </h1>
+            <hr />
+            <div>
+            <form>
+                <input className="input" type="text" value={sets} /><br/>
+              </form>  
+               <button name="1" onClick={handle}>1 </button>
+               <button name="2" onClick={handle}>2 </button>
+               <button name="3" onClick={handle}> 3</button>
+               <button name="+" onClick={handle}> +</button><br/>
+               <button name="4" onClick={handle}> 4</button>
+               <button name="5" onClick={handle}> 5</button>
+               <button name="6" onClick={handle}>6 </button>
+               <button name="-" onClick={handle}> -</button><br/>
+               <button name="7" onClick={handle}>7 </button>
+               <button name="8" onClick={handle}> 8</button>
+               <button name="9" onClick={handle}> 9</button>
+               <button name="/" onClick={handle}> /</button><br/>
+               <button name="0" onClick={handle}> 0</button>
+               <button id="cl" onClick={clears}>clear</button>
+               <button name="=" onClick={cal}> =</button>
+               <button name="*" onClick={handle}> *</button>
+               <button id="c" onClick={backs}> c</button>
+
+            </div>
+   </>
+   
   );
 }
 
